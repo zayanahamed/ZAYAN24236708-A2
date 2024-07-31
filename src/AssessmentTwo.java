9d7a97ebc1197adfe615d60e91dd6ccb76176eb0
zayanahamed <zayanahamed>
2024-08-01 03:16:01 +0500

    Working on the Part 1

---
+++ src/AssessmentTwo.java
@@ -0,0 +1,29 @@
+public class AssessmentTwo {
+    public static void main(String[] args) {
+        // Main method implementation
+    }
+
+    public void partThree() {
+        // Part three implementation
+    }
+
+    public void partFourA() {
+        // Part four A implementation
+    }
+
+    public void partFourB() {
+        // Part four B implementation
+    }
+
+    public void partFive() {
+        // Part five implementation
+    }
+
+    public void partSix() {
+        // Part six implementation
+    }
+
+    public void partSeven() {
+        // Part seven implementation
+    }
+}
