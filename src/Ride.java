1c3efd7d0b4ea513f2f7cb67329ae695f9b3e96a
zayanahamed <zayanahamed>
2024-08-01 03:15:00 +0500

    Working on the Part 1

--- src/Ride.java
+++ src/Ride.java
@@ -1,2 +1,45 @@
 public class Ride {
+    private String rideName;
+    private String rideType;
+    private Employee operator;
+
+    // Default constructor
+    public Ride() {}
+
+    // Parameterized constructor
+    public Ride(String rideName, String rideType, Employee operator) {
+        this.rideName = rideName;
+        this.rideType = rideType;
+        this.operator = operator;
+    }
+
+    // Getters and setters
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String rideName) {
+        this.rideName = rideName;
+    }
+
+    public String getRideType() {
+        return rideType;
+    }
+
+    public void setRideType(String rideType) {
+        this.rideType = rideType;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+    }
+
+    // Method to assign an Employee to operate the ride
+    public void assignOperator(Employee operator) {
+        this.operator = operator;
+    }
 }
