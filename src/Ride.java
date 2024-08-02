861351a4a022b3d6c9276766363a079ff91f88cd
zayanahamed <zayanahamed>
2024-08-02 12:02:22 +0500

    Working on the Part 2

--- src/Ride.java
+++ src/Ride.java
@@ -1,16 +1,26 @@
-public class Ride {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ride implements RideInterface {
     private String rideName;
     private String rideType;
     private Employee operator;
+    private List<Visitor> queue;
+    private List<Visitor> rideHistory;
 
     // Default constructor
-    public Ride() {}
+    public Ride() {
+        this.queue = new ArrayList<>();
+        this.rideHistory = new ArrayList<>();
+    }
 
     // Parameterized constructor
     public Ride(String rideName, String rideType, Employee operator) {
         this.rideName = rideName;
         this.rideType = rideType;
         this.operator = operator;
+        this.queue = new ArrayList<>();
+        this.rideHistory = new ArrayList<>();
     }
 
     // Getters and setters
@@ -41,5 +51,45 @@ public class Ride {
     // Method to assign an Employee to operate the ride
     public void assignOperator(Employee operator) {
         this.operator = operator;
+    }
+
+    // Interface methods
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        queue.add(visitor);
+    }
+
+    @Override
+    public void removeVisitorFromQueue(Visitor visitor) {
+        queue.remove(visitor);
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Queue: ");
+        for (Visitor v : queue) {
+            System.out.println(v.getName());
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        System.out.println("Running one cycle of the ride...");
+        // Simulate one cycle of the ride
+        if (!queue.isEmpty()) {
+            Visitor visitor = queue.remove(0);
+            rideHistory.add(visitor);
+            System.out.println("Visitor " + visitor.getName() + " took the ride.");
+        } else {
+            System.out.println("No visitors in the queue.");
+        }
+    }
+
+    @Override
+    public void printRideHistory() {
+        System.out.println("Ride history: ");
+        for (Visitor v : rideHistory) {
+            System.out.println(v.getName());
+        }
     }
 }
