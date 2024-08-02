19dea4f94f0c8a90b1991a0bad3814d5662c3437
zayanahamed <zayanahamed>
2024-08-02 12:01:22 +0500

    Working on the Part 2

--- src/RideInterface.java
+++
@@ -1,7 +0,0 @@
-public interface RideInterface {
-    void addVisitorToQueue(Visitor visitor);
-    void removeVisitorFromQueue(Visitor visitor);
-    void printQueue();
-    void runOneCycle();
-    void printRideHistory();
-}
